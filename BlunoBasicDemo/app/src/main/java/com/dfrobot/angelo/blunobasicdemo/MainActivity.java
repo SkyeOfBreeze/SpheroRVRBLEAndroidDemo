a125729e029e736c135502e080bf0cdbfc9048fa
Brendon Telman <b.telman01@gmail.com>
2019-10-27 11:45:05 -0500

    Remove kotlin as this demo doesn't use it

--- BlunoBasicDemo/app/src/main/java/com/dfrobot/angelo/blunobasicdemo/MainActivity.java
+++ BlunoBasicDemo/app/src/main/java/com/dfrobot/angelo/blunobasicdemo/MainActivity.java
@@ -1,12 +1,10 @@
 package com.dfrobot.angelo.blunobasicdemo;
 
-import android.os.Bundle;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ScrollView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
